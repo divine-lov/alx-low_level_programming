5a00c3ac87902fc11e7a8fad3f3b6cb887372456
divine-lov <levonluciano@gmail.com>
2023-07-13 13:38:41 -0700

    Updated files: 0x00-hello_world/4-puts.c 0x00-hello_world/a.out README.md

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  * return: 0 if exited properly, non zero otherwise
  */
 int main(void)
-i{
-	puts(""Programming is like building a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
