28bf99d96a932f55b15c45b3ae0f81b8178737b8
divine-lov <levonluciano@gmail.com>
2023-07-27 12:38:11 -0700

    Updated files: 0x06-pointers_arrays_strings/0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,19 +6,19 @@
  * @src: pointer source input
  *
  * Return: pointer to resulting string @dest
- */
+*/
 
 char *_strcat(char *dest, char *src)
 {
-    int c, c2;
+	int c, c2;
 
-    c = 0;
+	c = 0;
 
-    while (dest[c])
-        c++;
+	while (dest[c])
+		c++;
 
-    for (c2 = 0; src[c2]; c2++)
-        dest[c++] = src[c2];
+	for (c2 = 0; src[c2] ; c2++)
+		dest[c++] = src[c2];
 
-    return (dest);
+	return (dest);
 }
