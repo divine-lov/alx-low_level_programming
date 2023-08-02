e8f3b4a7ac07d3c29f661bfa565643bf6bda7072
divine-lov <levonluciano@gmail.com>
2023-08-02 13:36:23 -0700

    Updated files: 0x08-recursion/0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,19 @@
 #include "main.h"
-
 /**
- * _puts_recursion - prints a string
+ * _puts_recursion - print a string
  * @s:string
- * Return: void
+ * Return:void
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1};
-	}
-	else
-	{
+if (*s)
+{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+else
+{
 	_putchar('\n');
-	}
+}
+
 }
